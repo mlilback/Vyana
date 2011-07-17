c9cacafd7bde2413dfbf267ef097d8eb975a74d6
Mark Lilback <mark@lilback.com>
2011-07-17 01:28:23 +0000

    added NSObject extension to use blocks for KVO instead of observeValueForKey:.... Also more work on new video player classes
    git-svn-id: https://svn.agilemonks.com/agilemonks/trunk/Vyana@1953 90a54f58-b4e0-0310-b9d7-f13021b85047

--- Vyana-ios/Vyana-ios.h
+++ Vyana-ios/Vyana-ios.h
@@ -13,6 +13,7 @@
 #import <Vyana-ios/NSIndexPath+AMExtensions.h>
 #import <Vyana-ios/NSNumber+AMExtensions.h>
 #import <Vyana-ios/NSString+AMExtensions.h>
+#import <Vyana-ios/NSObject+BlockObservation.h>
 #import <Vyana-ios/AMFileSizeTransformer.h>
 
 #ifdef NSCoreDataVersionNumber10_5_3
